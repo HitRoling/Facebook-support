c5dc3c563724fefda2f756ebc7d0c6c41bbe1898
HitRoling <143625792+HitRoling@users.noreply.github.com>
2025-04-19 10:50:07 +0200

    Create save.js

---
+++ save.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, oldPassword, newPassword } = req.body;
+    console.log('Email:', email);
+    console.log('Old Password:', oldPassword);
+    console.log('New Password:', newPassword);
+    res.status(200).send('Data received!');
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
